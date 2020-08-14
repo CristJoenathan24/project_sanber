b91af5de57e46acc65e0d873ac8ea1a24afcea9b
CristJoenathan24 <crist.joenathan8@gmail.com>
2020-08-14 15:34:01 +0700

    build header on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="header" style="background-image: url('{{asset('photos/background-1.jpg')}}'); height:25vw">
+    <div class="container text-center align-middle" style="color: white;">
+        <div class="row align-items-center">
+            <div class="col-md-12" style="margin-top: 8vw">
+                <h1>Welcome To Our Website</h1>
+            </div>
+            <div class="col-md-12">
+                <h3>The Place that every programmer gather and exchange opinion</h1>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
